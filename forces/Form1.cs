e0294700d1f83f71973d961bf99bc4b84130ba3a
John Speed <rc0029514@student.riversidecollege.ac.uk>
2019-03-12 10:46:16 +0000

    jj

--- forces/Form1.cs
+++ forces/Form1.cs
@@ -19,21 +19,47 @@ namespace forces
 
         private void Form1_Load(object sender, EventArgs e)
         {
+
+        }
+
+        double sin(double x)
+        {
             return (Math.Sin(x * Math.PI / 180.0));
         }
-        Double asin(double x)
+        double asin(double x)
         {
-            return (Math.Asin(x * 180.0 /Math.PI));
+            return (Math.Asin(x * 180.0 / Math.PI));
         }
-        Double cos(double x)
+        double cos(double x)
         {
             return (Math.Cos(x * Math.PI / 180.0));
         }
-        Double acos(double x)
+        double acos(double x)
         {
-            return (Math.acos(x) * 180.0 / Math.PI);
+            return (Math.Acos(x) * 180.0 / Math.PI);
+        }
+ 
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double Force = double.Parse(textBox1.Text);
+            double angle = double.Parse(textBox2.Text);
+            double Fx = Force * cos(angle);
+            double Fy = angle * sin(Force);
+
+            MessageBox.Show("Fx =" + (Fx.ToString("N3") + "\n"));
+            MessageBox.Show("Fy =" + (Fx.ToString("N3") + "\n"));
         }
     }
 
 }
- {
+ 
